86adee6f27513b8604e33b5bd5c330b0f37d7115
Mathijs de Langen <langen@robot-rose.nl>
2014-12-08 16:13:19 +0100

    created arm_controller core file

---
+++ arm_controller_core/include/arm_controller/arm_controller.hpp
@@ -0,0 +1,12 @@
+/***********************************************************************************
+* Copyright: Rose B.V. (2014)
+*
+* Revision History:
+*	Author: Mathijs de Langen
+*	Date  : 2014/12/08
+* 		- File created.
+*
+* Description:
+*	The generic arm controller
+* 
+***********************************************************************************/
---
+++ arm_controller_core/include/arm_controller/arm_controller_node.hpp
@@ -0,0 +1,25 @@
+/***********************************************************************************
+* Copyright: Rose B.V. (2014)
+*
+* Revision History:
+*	Author: Mathijs de Langen
+*	Date  : 2014/12/08
+* 		- File created.
+*
+* Description:
+*	Arm controller node. Starts the correct arm_controller.
+* 
+***********************************************************************************/
+#ifndef ARM_CONTROLLER_NODE_HPP
+#define ARM_CONTROLLER_NODE_HPP
+
+#include <iostream>
+#include <pluginlib/class_loader.h>
+#include <pluginlib_tutorials_/polygon_base.h>
+#include <ros/ros.h>
+#include <stdio.h>
+#include <vector>
+
+#include "rose_arm_controller/arm_controller_base.hpp"
+
+#endif // ARM_CONTROLLER_NODE_HPP
